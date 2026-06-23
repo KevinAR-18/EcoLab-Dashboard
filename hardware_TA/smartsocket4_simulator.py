4eeb96603988b1b48bb0e3a17b77b6430441b7e3
KevinAR <kevinandika18@gmail.com>
2026-06-23 11:49:48 +0700

    feat: update MQTT configuration for smartsocket simulators with new broker and credentials

--- hardware_TA/smartsocket4_simulator.py
+++ hardware_TA/smartsocket4_simulator.py
@@ -26,13 +26,13 @@ import paho.mqtt.client as mqtt
 # ============================================================
 # CONFIG
 # ============================================================
-# MQTT_BROKER = "10.33.11.148"
-MQTT_BROKER = "DESKTOP-CVPE153"
+MQTT_BROKER = "10.33.11.148"
+# MQTT_BROKER = "DESKTOP-CVPE153"
 MQTT_PORT = 8883  # TLS
-MQTT_USERNAME = ""
-MQTT_PASSWORD = ""
-# CA_CERT = os.path.join(os.path.dirname(os.path.dirname(__file__)), "credentials", "ca.crt")
-CA_CERT = os.path.join(os.path.dirname(os.path.dirname(__file__)), "credentials", "ca2.crt")
+MQTT_USERNAME = "smartsocket4"
+MQTT_PASSWORD = "smart4"
+CA_CERT = os.path.join(os.path.dirname(os.path.dirname(__file__)), "credentials", "ca.crt")
+# CA_CERT = os.path.join(os.path.dirname(os.path.dirname(__file__)), "credentials", "ca2.crt")
 
 # Topics
 TOPIC_CONTROL = "ecolab/socket/4/control"
